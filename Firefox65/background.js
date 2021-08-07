cec81768b8d0cf16ad175df4b9e966f407e6b092
armagedescu <armagedescu@gmail.com>
2021-08-07 21:41:58 +0300

    Porting to new version of Firefox

--- Firefox65/background.js
+++ Firefox65/background.js
@@ -1,9 +1,9 @@
 {//Firefox
-   console.log("salut");
+   console.log("Creating context menus");
    var id      = chrome.contextMenus.create({"title": "Roattta waayyy!!!!", "contexts":["selection", "page"]});
    var childId = chrome.contextMenus.create({"title": "Play Small",         "contexts":["selection", "page"], "parentId": id, "onclick": menuCommandPlaySmallBoard});
    var childId = chrome.contextMenus.create({"title": "Play Medium",        "contexts":["selection", "page"], "parentId": id, "onclick": menuCommandPlayMediumBoard});
-   console.log("add context menus");
+   console.log("Context menus created");
 }
 
 function menuCommandPlaySmallBoard (info, tab)
@@ -21,73 +21,97 @@ function menuCommandPlayMediumBoard (info, tab)
 {
    try
    {
-      playBoard   (info, tab, "status = 1, height = 450, width = 450, resizable = 0, scrollbars=0", "medium35");
+       playBoard   (info, tab, "status = 1, height = 450, width = 450, resizable = 0, scrollbars=0", "medium35");
    }
    catch (err)
    {
    }
 }
 
-function onCreated(windowInfo) {
-  console.log(`Created window: ${windowInfo.id}`);
+function handleResponse(message)
+{
+   console.log("background.handleResponse");
+   console.log(`background script sent a response: ${message.response}`);
+   console.log("background.handleResponse end");
+}
+
+function handleError(error)
+{
+   console.log("background.handleError");
+   console.log(`Error: ${error}`);
+   console.log("background.handleError end");
+}
+
+function onCreated(windowInfo, request)
+{
+   console.log("background.sending onCreated ");
+
+   browser.windows.get(windowInfo.id).then
+   (
+      (wnd) =>
+      {
+		 console.log("background.onCreated Creation listener there is: "  + wnd.id);
+         //chrome.runtime.sendMessage
+         browser.runtime.sendMessage(request);
+         //({
+         //      chessObject:
+         //      {
+         //         gametype : "PGN_OR_FEN_board",
+         //         //content : selection,
+         //         //imgPath : imagePath
+         //      }
+         //});
+		 console.log("background.onCreated sent message");
+      }
+   );
+
+   console.log("background.sending onCreated end");
 }
 
 function onError(error) {
-  console.log(`Error: ${error}`);
+  console.log("background.sending onError");
+  //console.log(`Error: ${error}`);
+  console.log("background.sending onError end");
 }
+
 function playBoard (info, tab, windowAttributes, imagePath)
 {
+   let selection = info.selectionText;
    var createData = {
-     type: "detached_panel",
-     url: "pop.html?boardType=zzzzz&PGN=" + info.selectionText,
-     //url: "pop.html",
-     width: 250,
-     height: 100
+      type  : "detached_panel",
+      url   : "pop.html?boardType=" + imagePath + "&PGN=" + encodeURIComponent(info.selectionText),
+      width : 250,
+      height: 100
+
    };
+
+   let request =
+      {
+         chessObject:
+         {
+            gametype : "PGN_OR_FEN_board",
+            content : selection,
+            imgPath : imagePath
+         }
+      };
    try
    {
       console.log ("Play Board: " + imagePath);
       console.log (info.selectionText);
-      console.log ("Extension URL: " + browser.extension.getURL("pop.html?boardType=zzzzz&PGN=" + info.selectionText));
-	  //var pop = window.open ("about:blank", "zzz");
-	  //var pop = window.open (browser.extension.getURL("pop.html"), "zzz");
-	  var creating = browser.windows.create(createData);
-	  creating.then(onCreated, onError);
-	  console.log (creating);
-	  //var pop = window.open (browser.extension.getURL("pop.html"), "zzz", "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes");
-	  //console.log (pop);
-	  //var pop = window.open ("pop.html", "zzz", "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes");
-	  //var pop = window.open ("https://www.newsru.com", "zzz", "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes");
-	  
-	  console.log ("opened pop.html");
-      //open("pop.html", "myWindow", windowAttributes);
-      //open("board.html", "myWindow", windowAttributes);
-      //setTimeout(function()
-	  //     {
-      //        try
-      //        {
-	  //           var selection = "";
-	  //           if (info.selectionText) selection = info.selectionText;
-      //           chrome.extension.sendRequest
-      //              (
-      //                 {
-      //                    chessObject:
-      //                    {
-      //                       gametype : "PGN_OR_FEN_board",
-      //                       content : selection,
-      //                       imgPath : imagePath
-      //                    }
-      //                 },
-      //                 function(response)
-      //                 {
-      //                    console.log(response.info);
-      //                 }
-      //              );
-      //        }catch(err){}
-      //     }, 100);
+      console.log ("Extension URL: " + browser.runtime.getURL("pop.html?boardType=" + imagePath + "&PGN=" + encodeURIComponent(info.selectionText)));
+      console.log ("Extension URL: " + browser.runtime.getURL("pop.html?boardType=" + imagePath + "&PGN=" + decodeURIComponent(encodeURIComponent(info.selectionText))));
+
+      let creating = browser.windows.create(createData);
+      creating.then((inf) => {onCreated (inf, request)}, onError);
+
+
+      console.log ("opened pop.html");
+
    }
    catch (err)
    {
+      console.log ("error, not opened");
+      console.log ("main background playBoard()> " + err);
       throw "main background playBoard()> " + err;
    }
 }

c4e9627bbd4eafe1ff42c45bbc9c4b29e5a032a7
armagedescu <armagedescu@gmail.com>
2021-08-10 23:53:47 +0300

    tmp

--- Firefox65/create_xpi.cmd
+++ Firefox65/create_xpi.cmd
@@ -4,7 +4,7 @@
 @if not exist %zip7zPath% set zip7zPath="%ProgramFiles(x86)%\7-Zip\7z.exe"
 @if not exist %zip7zPath% set zip7zPath="7z.exe"
 @if not exist %zip7zPath% goto end
-%zip7zPath% a -tzip .\ChessAddIn.zip  -xr!.svn -xr!*.bak -xr!btn*.gif -xr!Thumbs.db -xr!IVFFirefoxChess.js -xr!options.js -xr!*snow_queen* -xr!defaults .\chrome.manifest .\install.rdf .\chrome .\defaults
+%zip7zPath% a -tzip .\ChessAddIn.zip  -xr!.svn -xr!*.bak -xr!Thumbs.db -xr!defaults *.js *.html .\manifest.json .\icons .\medium35 .\mini18
 @copy .\ChessAddIn.zip .\ChessAddIn.xpi
 @goto finish
 :end

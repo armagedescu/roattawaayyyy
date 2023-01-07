7ae3f3c5c6f2aac501a96e619a54a2529eb32b7d
Ion Filipski <armagedescu@gmail.com>
2023-01-07 14:01:56 +0200

    Continue refactoring

--- Firefox65/board_main.js
+++ Firefox65/board_main.js
@@ -31,12 +31,12 @@ function drop(ev, chessGame)
    {
       ev.preventDefault(); //TODO: check if need to prevent default
       var data = ev.dataTransfer.getData("Text");  //let's keep in source/target communication style
-      chessGame.BoardClick.call (chessGame, data,         true); //move from.id, d&d source
-      chessGame.BoardClick.call (chessGame, ev.target.id, true); //move to.id, d&d target
+      chessGame.BoardClick (data,         true); //move from.id, d&d source
+      chessGame.BoardClick (ev.target.id, true); //move to.id, d&d target
    }
    catch (err)
    {
-	  console.log("board_doc_main:drop> " + err);
+	  console.log ("board_doc_main:drop> " + err);
    }
 }
 
@@ -44,11 +44,11 @@ function cellClick(ev, chessGame)
 {
    try
    {
-      chessGame.BoardClick.call(chessGame, ev.target.id, true); //"this" reffers to cellClick owner, = the img element
+      chessGame.BoardClick (ev.target.id, true);
    }
    catch (err)
    {
-	  console.log("board_doc_main:cellClick> " + err);
+	  console.log ("board_doc_main:cellClick> " + err);
    }
 }
 
@@ -76,28 +76,28 @@ function updateListeners(chessGame)
    try
    {
       //TODO: buttons to be added dynamically
-      document.body.addEventListener ('keydown', function (ev) {bodyKeyPress(ev, currentGame);}, false);
+      document.body.addEventListener ('keydown', (ev) => {bodyKeyPress(ev, currentGame);}, false);
 
-      document.getElementById ( 'btnFlipBoard'   ).addEventListener      ( 'click', function() {currentGame.btnFlipBoardListener.call(currentGame);},    false  );
-      document.getElementById ( 'btnInit'        ).addEventListener      ( 'click', function() {currentGame.btnInitListener.call(currentGame);},         false  );
-      document.getElementById ( 'btnMoveBack'    ).addEventListener      ( 'click', function() {currentGame.btnMoveBackListener.call(currentGame);},     false  );
-      document.getElementById ( 'btnMoveForward' ).addEventListener      ( 'click', function() {currentGame.btnMoveForwardListener.call(currentGame);},  false  );
-      document.getElementById ( 'btnMoveLast'    ).addEventListener      ( 'click', function() {currentGame.btnMoveLastListener.call(currentGame);},     false  );
-      document.getElementById ( 'btnGetFEN'      ).addEventListener      ( 'click', function() {currentGame.btnGetFENListener.call(currentGame);},       false  );
-      document.getElementById ( 'btnShowFENList' ).addEventListener      ( 'click', function() {currentGame.btnShowFENListListener.call(currentGame);},  false  );
-      document.getElementById ( 'btnPlay'        ).addEventListener      ( 'click', function() {currentGame.btnPlayListener.call(currentGame);},         false  );
+      document.getElementById ( 'btnFlipBoard'   ).addEventListener      ( 'click', () => {currentGame.btnFlipBoardListener();  },  false  );
+      document.getElementById ( 'btnInit'        ).addEventListener      ( 'click', () => {currentGame.btnInitListener();       },  false  );
+      document.getElementById ( 'btnMoveBack'    ).addEventListener      ( 'click', () => {currentGame.btnMoveBackListener();   },  false  );
+      document.getElementById ( 'btnMoveForward' ).addEventListener      ( 'click', () => {currentGame.btnMoveForwardListener();},  false  );
+      document.getElementById ( 'btnMoveLast'    ).addEventListener      ( 'click', () => {currentGame.btnMoveLastListener();   },  false  );
+      document.getElementById ( 'btnGetFEN'      ).addEventListener      ( 'click', () => {currentGame.btnGetFENListener();     },  false  );
+      document.getElementById ( 'btnShowFENList' ).addEventListener      ( 'click', () => {currentGame.btnShowFENListListener();},  false  );
+      document.getElementById ( 'btnPlay'        ).addEventListener      ( 'click', () => {currentGame.btnPlayListener();       },  false  );
 
       var celId = "";
       for (ii = 0; ii < 64; ii++)
       {
          cellId = "" + ii;
          cell = document.getElementById(cellId);
-         cell.addEventListener    ('click',     function (ev) {cellClick(ev, currentGame);}, false);
+         cell.addEventListener    ('click',     (ev) => {cellClick (ev, currentGame);}, false);
 
          //TODO: one more d&d handler may be needed here, as required by IE and maybe by Safari
-         cell.addEventListener    ('dragstart', function(ev){drag      (ev, currentGame);}, false);
-         cell.addEventListener    ('dragover',  function(ev){allowDrop (ev, currentGame);}, false);
-         cell.addEventListener    ('drop',      function(ev){drop      (ev, currentGame);}, false);
+         cell.addEventListener    ('dragstart', (ev) => {drag      (ev, currentGame);}, false);
+         cell.addEventListener    ('dragover',  (ev) => {allowDrop (ev, currentGame);}, false);
+         cell.addEventListener    ('drop',      (ev) => {drop      (ev, currentGame);}, false);
       }
    }
    catch (err)
@@ -111,10 +111,8 @@ function board_doc_main (chessObject)
    var chessGame = null;
    try
    {
-      var errDiv = document.getElementById("error_div");  
       var chessBoard = document.getElementById('chess_board');
-      chessGame = new IVFChessGame (chessObject.imgPath + "/", chessObject.content, updateListeners, errDiv, chessBoard);
-	  chessGame.variable_reset.call(chessGame);  //TODO: try less ctoring
+      chessGame = new IVFChessGame (chessObject.imgPath + "/", chessObject.content, updateListeners, chessBoard);
    }
    catch (err)
    {
